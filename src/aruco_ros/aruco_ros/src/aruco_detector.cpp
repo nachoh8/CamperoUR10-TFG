1948bb0e5e190c0136d80b76ee68c14fb44bb3d3
nacho <nachohseara@gmail.com>
2021-05-19 22:21:33 +0200

    transformacion punto a img original

--- src/aruco_ros/aruco_ros/src/aruco_detector.cpp
+++ src/aruco_ros/aruco_ros/src/aruco_detector.cpp
@@ -17,9 +17,6 @@
 #include <dynamic_reconfigure/server.h>
 #include <aruco_ros/ArucoThresholdConfig.h>
 
-/*#include <aruco_msgs/Marker.h>
-#include <aruco_msgs/MarkerArray.h>*/
-
 #include <cv_bridge/cv_bridge.h>
 
 #include <image_transport/image_transport.h>
@@ -32,6 +29,7 @@
 
 #include <campero_ur10_msgs/ArucoMarker.h>
 #include <campero_ur10_msgs/ArucoMarkerArray.h>
+#include <campero_ur10_msgs/ArucoMarkersImg.h>
 
 using namespace aruco;
 
@@ -47,7 +45,9 @@ private:
   bool cam_info_received;
   
   image_transport::Publisher image_detector_pub;
-  image_transport::Publisher image_res_pub;
+
+  // Cuano encuentra 4 marcas publica las marcas y la imagen original
+  // image_transport::Publisher image_res_pub;
   ros::Publisher markers_pub;
   
   std::string marker_frame;
@@ -100,8 +100,9 @@ public:
     cam_info_sub = nh.subscribe("/camera_info", 1, &ArucoDetector::cam_info_callback, this);
 
     image_detector_pub = it.advertise("detector", 1);
-    image_res_pub = it.advertise("result", 1);
-    markers_pub = nh.advertise<campero_ur10_msgs::ArucoMarkerArray>("markers", 100);
+    //image_res_pub = it.advertise("result", 1);
+    //markers_pub = nh.advertise<campero_ur10_msgs::ArucoMarkerArray>("markers", 100);
+    markers_pub = nh.advertise<campero_ur10_msgs::ArucoMarkersImg>("markers_img", 1);
 
     nh.param<double>("marker_size", marker_size, 0.05);
     nh.param<std::string>("reference_frame", reference_frame, "");
@@ -122,150 +123,6 @@ public:
     dyn_rec_server.setCallback(boost::bind(&ArucoDetector::reconf_callback, this, _1, _2));
   }
 
-
-  /*void filterRectPoints(std::vector<cv::Point2f>& pts, std::vector<cv::Point2f>& res) {
-    const int len = pts.size();
-    if (len == 4) return;
-
-    std::vector< std::pair<double, int> > sum_v(len);
-    std::vector< std::pair<double, int> > diff_v(len);
-    for (int i = 0; i < len; i++) {
-      sum_v[i] = std::make_pair(pts[i].x + pts[i].y, i);
-      diff_v[i] = std::make_pair(pts[i].y - pts[i].x, i);
-    }
-
-    std::sort(sum_v.begin(), sum_v.end());
-    std::sort(diff_v.begin(), diff_v.end());
-
-    // top-left point min sum -> idx 0
-	  // bottom-right point max sum -> idx 2
-    // top-right point min diff -> idx 1
-	  // bottom-left max diff -> idx 3
-    res.push_back(pts[sum_v[0].second]);
-    res.push_back(pts[diff_v[0].second]);
-    res.push_back(pts[sum_v[sum_v.size()-1].second]);
-    res.push_back(pts[diff_v[diff_v.size()-1].second]);
-  }*/
-  
-  /*void correctImage(std::vector<cv::Point2f>& marker_pts, cv::Mat& image) {
-    // WARNING: si se hace con hull los puntos deben ser Point (tipo entero)
-    //std::vector< std::vector<cv::Point> > hull(1);
-    //cv::convexHull(cv::Mat(marker_pts), hull[0], false);
-    
-    // Get Corner points
-    // filterRectPoints(hull[0]);
-    std::vector<cv::Point2f> src_pts;
-    filterRectPoints(marker_pts, src_pts);
-
-    cv::Point2f tl = src_pts[0];//hull[0][0];
-    cv::Point2f tr = src_pts[1];//hull[0][1];
-    cv::Point2f br = src_pts[2];//hull[0][2];
-    cv::Point2f bl = src_pts[3];//hull[0][3];
-
-    // Draw Convex Shape
-    // cv::drawContours( inImage, hull, 0, cv::Scalar(0,255,0));
-    /*cv::line(image, tl, tr, cv::Scalar(255,0,0), 2);
-    cv::line(image, tl, bl, cv::Scalar(255,0,0), 2);
-    cv::line(image, tr, br, cv::Scalar(255,0,0), 2);
-    cv::line(image, bl, br, cv::Scalar(255,0,0), 2);
-  
-    // Draw convex points
-    /*for (int i = 0; i < hull[0].size(); i++) {
-      std::cout << hull[0][i] << std::endl;
-      cv::circle(image, hull[0][i], 10, cv::Scalar(0,0,0), -1);
-    }
-
-    // Get Warped Points
-    double width_top = cv::norm(tr - tl);
-    double width_bottom = cv::norm(br - bl);
-    double max_W = width_top > width_bottom ? width_top : width_bottom;
-    // std::cout << width_top << " " << width_bottom << std::endl;
-
-    double height_left = cv::norm(bl - tl);
-    double height_right = cv::norm(br - tr);
-    double max_H = height_left > height_right ? height_left : height_right;
-    // std::cout << height_left << " " << height_right << std::endl;
-
-    std::vector<cv::Point2f> dst_pts;
-    dst_pts.push_back(cv::Point2f(0, 0));
-    dst_pts.push_back(cv::Point2f(max_W-1, 0));
-    dst_pts.push_back(cv::Point2f(max_W-1, max_H-1));
-    dst_pts.push_back(cv::Point2f(0, max_H-1));
-    
-    // Transform image
-    cv::Mat H = cv::getPerspectiveTransform(src_pts, dst_pts);
-    cv::warpPerspective(image, image, H, cv::Size(max_W, max_H));
-  }*/
-
-  void filterRectPoints(std::vector<cv::Point2f>& pts, std::vector<cv::Point2f>& res) {
-    const int len = pts.size();
-    if (len == 4) return;
-
-    std::vector< std::pair<double, int> > sum_v(len);
-    std::vector< std::pair<double, int> > diff_v(len);
-    for (int i = 0; i < len; i++) {
-      sum_v[i] = std::make_pair(pts[i].x + pts[i].y, i);
-      diff_v[i] = std::make_pair(pts[i].y - pts[i].x, i);
-    }
-
-    std::sort(sum_v.begin(), sum_v.end());
-    std::sort(diff_v.begin(), diff_v.end());
-
-    // top-left point min sum -> idx 0
-	  // bottom-right point max sum -> idx 2
-    // top-right point min diff -> idx 1
-	  // bottom-left max diff -> idx 3
-    res.push_back(pts[sum_v[0].second]);
-    res.push_back(pts[diff_v[0].second]);
-    res.push_back(pts[sum_v[sum_v.size()-1].second]);
-    res.push_back(pts[diff_v[diff_v.size()-1].second]);
-  }
-  
-  void correctImage(std::vector< std::vector<cv::Point2f> >& marker_pts, cv::Mat& image) {
-    
-    std::vector< std::vector<cv::Point> > pts(marker_pts.size());
-    std::vector<cv::Point2f> pts_f;
-    for (int i = 0; i < marker_pts.size(); i++) {
-      for (int j = 0; j < marker_pts[i].size(); j++) {
-        pts[i].push_back(cv::Point(marker_pts[i][j].x, marker_pts[i][j].y));
-        pts_f.push_back(marker_pts[i][j]);
-      }
-    }
-    // Get Corner points
-    std::vector<cv::Point2f> src_pts;
-    filterRectPoints(pts_f, src_pts);
-
-    cv::Point2f tl = src_pts[0];//hull[0][0];
-    cv::Point2f tr = src_pts[1];//hull[0][1];
-    cv::Point2f br = src_pts[2];//hull[0][2];
-    cv::Point2f bl = src_pts[3];//hull[0][3];
-
-    // Draw Convex Shape
-    //cv::drawContours( image, pts, -1, cv::Scalar(255,255,255), 1, cv::FILLED);
-    cv::fillPoly(image, pts, cv::Scalar(255,255,255));
-
-    // Get Warped Points
-    double width_top = cv::norm(tr - tl);
-    double width_bottom = cv::norm(br - bl);
-    double max_W = width_top > width_bottom ? width_top : width_bottom;
-    // std::cout << width_top << " " << width_bottom << std::endl;
-
-    double height_left = cv::norm(bl - tl);
-    double height_right = cv::norm(br - tr);
-    double max_H = height_left > height_right ? height_left : height_right;
-    // std::cout << height_left << " " << height_right << std::endl;
-
-    std::vector<cv::Point2f> dst_pts;
-    dst_pts.push_back(cv::Point2f(0, 0));
-    dst_pts.push_back(cv::Point2f(max_W-1, 0));
-    dst_pts.push_back(cv::Point2f(max_W-1, max_H-1));
-    dst_pts.push_back(cv::Point2f(0, max_H-1));
-    
-    // Transform image
-    cv::Mat H = cv::getPerspectiveTransform(src_pts, dst_pts);
-    cv::warpPerspective(image, image, H, cv::Size(max_W, max_H));
-  }
-  
   bool getTransform(const std::string& refFrame,
                     const std::string& childFrame,
                     tf::StampedTransform& transform)
@@ -324,15 +181,15 @@ public:
         std::vector<Marker> markers;
         mDetector.detect(image_detector, markers, camParam, marker_size, false);
 
-        // Init MarkerArray msg
-        campero_ur10_msgs::ArucoMarkerArray marker_array;
+        // Init ArucoMarkersImg msg
+        campero_ur10_msgs::ArucoMarkersImg marker_array;
 
         // Point Set of Marker Points
         std::vector< std::vector<cv::Point2f> > marker_pts;
 
         // for each marker, draw info and its boundaries in the image
         for (size_t i = 0; i < markers.size(); ++i) {
-          // 1.Build Marker msg
+          // 1.Get marker transform
           tf::Transform transform = aruco_ros::arucoMarker2Tf(markers[i], rotate_marker_axis_);
           tf::StampedTransform cameraToReference;
           cameraToReference.setIdentity();
@@ -354,13 +211,10 @@ public:
                                                 reference_frame, marker_frame_final);
           br.sendTransform(stampedTransform);
           
+          // 2.Build ArucoMarker msg and Add to ArucoMarkerArray msg
           campero_ur10_msgs::ArucoMarker marker_msg;
           tf::poseTFToMsg(transform, marker_msg.pose);
-          //tf::poseTFToMsg(transform, marker_msg.pose.pose);
           
-          // marker_msg.header = marker_array.header;
-          /*marker_msg.header.frame_id = marker_frame_final;
-          marker_msg.header.stamp = curr_stamp;*/
           for (int j = 0; j < markers[i].size(); j++) {
             geometry_msgs::Point32 pt;
             pt.x = markers[i][j].x;
@@ -369,34 +223,37 @@ public:
           }
           marker_msg.id = markers[i].id;
 
-          // Add to MarkerArray
-          marker_array.markers.push_back(marker_msg);
+          
+          marker_array.markers.markers.push_back(marker_msg);
 
-          // 2.Draw Marker on image
+          // 3.Draw Marker on image
           markers[i].draw(image_detector,cv::Scalar(0,0,255),2);
 
-          // 3.Draw a 3d cube if there is 3d info
+          // 4.Draw a 3d cube if there is 3d info
           if (camParam.isValid() && marker_size > 0) {
             CvDrawingUtils::draw3dAxis(image_detector, markers[i], camParam);
           }
 
-          // 4.Add Marker Image Points to Point Set
+          // 5.Add Marker Image Points to Point Set
           marker_pts.push_back(markers[i]);
         }
 
-        // Publish MarkerArray
-        markers_pub.publish(marker_array);
         
-        // Get Convex Hull
+        // Num Markers is correct
         if (markers.size() == 4) {
-          cv::Mat image_res = cv_ptr->image;
-          correctImage(marker_pts, image_res);
+          /*cv::Mat image_res = cv_ptr->image;
+          correctImage(marker_pts, image_res);*/
 
-          cv_bridge::CvImage out_msg;
+          // Publish ArucoMarkerArray with Original Image
+          marker_array.img = *(cv_ptr->toImageMsg());
+          markers_pub.publish(marker_array);
+
+          // Publish Original Image
+          /*cv_bridge::CvImage out_msg;
           out_msg.header.stamp = curr_stamp;
           out_msg.encoding = sensor_msgs::image_encodings::RGB8;
-          out_msg.image = image_res;
-          image_res_pub.publish(out_msg.toImageMsg());
+          out_msg.image = cv_ptr->image;
+          image_res_pub.publish(out_msg.toImageMsg());*/
         }
 
         // Publish Image
@@ -433,7 +290,6 @@ public:
     cam_info_received = true;
     cam_info_sub.shutdown();
   }
-
 
   void reconf_callback(aruco_ros::ArucoThresholdConfig &config, uint32_t level)
   {
--- src/campero_ur10/image_inpainting/src/image_inpainting.cpp
+++ src/campero_ur10/image_inpainting/src/image_inpainting.cpp
@@ -1,37 +1,235 @@
-#include <ros/ros.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <ros/ros.h> 
+
+#include <tf/tf.h>
+
 #include <image_transport/image_transport.h> // permite suscribirse/publicar en canales de imagen
-#include <cv_bridge/cv_bridge.h> // puente entre OpenCV y sensor_msgs/Image
 #include <sensor_msgs/image_encodings.h>
+
+#include <geometry_msgs/Pose.h>
+
+#include <cv_bridge/cv_bridge.h> // puente entre OpenCV y sensor_msgs/Image
+
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-static const std::string OPENCV_WINDOW = "Image window";
+#include <campero_ur10_msgs/ArucoMarker.h>
+#include <campero_ur10_msgs/ArucoMarkerArray.h>
+#include <campero_ur10_msgs/ArucoMarkersImg.h>
 
 class ImageInpainting
 {
+private:
+    /// Ros
+
     ros::NodeHandle nh;
     image_transport::ImageTransport it;
 
-    // aruco markers
-    ros::Subscriber aruco_sub;
-
-    // Image Raw
-    image_transport::Subscriber image_sub;
+    // aruco markers and image
+    ros::Subscriber markers_img_sub;
 
     // Result Image
     image_transport::Publisher image_res_pub;
 
+    // Debug Image
+    image_transport::Publisher image_debug_pub;
+
+    /// Image Procesing
+    cv::Mat H; // homografy matrix
+    
+
 public:
     ImageInpainting() : it(nh) {
-        image_sub = it.subscribe("/aruco_detector/image_raw", 1, &ImageInpainting::image_callback, this);
+        image_debug_pub = it.advertise("/image_inpainting/image_debug", 1);
         image_res_pub = it.advertise("/image_inpainting/image_res", 1);
-        //aruco_sub = nh.subscribe("/camera_info", 1, &ArucoDetector::cam_info_callback, this);
+        markers_img_sub = nh.subscribe("/aruco_detector/markers_img", 1, &ImageInpainting::markers_img_callback, this);
     }
 
     ~ImageInpainting(){}
 
-    void image_callback(const sensor_msgs::ImageConstPtr& msg) {
+    cv::Point ptTfPerspective(cv::Point pt) {
+        cv::Point3d p_src(pt.x, pt.y, 1);
+        cv::Point3d p_dst(cv::Mat(H.inv() * cv::Mat(p_src)));
+        p_dst /= p_dst.z; // normalize
 
+        return cv::Point(p_dst.x, p_dst.y);
+    }
+
+    geometry_msgs::Pose pt2Pose(cv::Point pt) {
+
+    }
+
+
+    void filterRectPoints(std::vector<cv::Point2f>& pts, std::vector<cv::Point2f>& res) {
+        const int len = pts.size();
+        if (len == 4) return;
+
+        std::vector< std::pair<double, int> > sum_v(len);
+        std::vector< std::pair<double, int> > diff_v(len);
+        for (int i = 0; i < len; i++) {
+            sum_v[i] = std::make_pair(pts[i].x + pts[i].y, i);
+            diff_v[i] = std::make_pair(pts[i].y - pts[i].x, i);
+        }
+
+        std::sort(sum_v.begin(), sum_v.end());
+        std::sort(diff_v.begin(), diff_v.end());
+
+        // top-left point min sum -> idx 0
+        // bottom-right point max sum -> idx 2
+        // top-right point min diff -> idx 1
+        // bottom-left max diff -> idx 3
+        res.push_back(pts[sum_v[0].second]);
+        res.push_back(pts[diff_v[0].second]);
+        res.push_back(pts[sum_v[sum_v.size()-1].second]);
+        res.push_back(pts[diff_v[diff_v.size()-1].second]);
+    }
+    
+    void correctImage(std::vector<campero_ur10_msgs::ArucoMarker>& markers, cv::Mat& image) {
+        
+        // Cast to cv::Point/2f type
+        std::vector< std::vector<cv::Point> > pts(markers.size());
+        std::vector<cv::Point2f> pts_f;
+        for (int i = 0; i < markers.size(); i++) {
+            for (int j = 0; j < markers[i].img_points.size(); j++) {
+                pts[i].push_back(cv::Point(markers[i].img_points[j].x, markers[i].img_points[j].y));
+                pts_f.push_back(cv::Point2f(markers[i].img_points[j].x, markers[i].img_points[j].y));
+            }
+        }
+
+        // Delete Markers
+        cv::fillPoly(image, pts, cv::Scalar(255,255,255));
+        
+        // Get Corner Source points
+        std::vector<cv::Point2f> src_pts;
+        filterRectPoints(pts_f, src_pts);
+
+        cv::Point2f tl = src_pts[0];
+        cv::Point2f tr = src_pts[1];
+        cv::Point2f br = src_pts[2];
+        cv::Point2f bl = src_pts[3];
+
+        // Get Warped Destination Points
+        double width_top = cv::norm(tr - tl);
+        double width_bottom = cv::norm(br - bl);
+        double max_W = width_top > width_bottom ? width_top : width_bottom;
+
+        double height_left = cv::norm(bl - tl);
+        double height_right = cv::norm(br - tr);
+        double max_H = height_left > height_right ? height_left : height_right;
+
+        std::vector<cv::Point2f> dst_pts;
+        dst_pts.push_back(cv::Point2f(0, 0));
+        dst_pts.push_back(cv::Point2f(max_W-1, 0));
+        dst_pts.push_back(cv::Point2f(max_W-1, max_H-1));
+        dst_pts.push_back(cv::Point2f(0, max_H-1));
+        
+        // Transform image
+        H = cv::getPerspectiveTransform(src_pts, dst_pts);
+        cv::warpPerspective(image, image, H, cv::Size(max_W, max_H));
+    }
+    
+    cv::Point test(cv::Mat& img, cv::Mat& img_correct) {
+        cv::Mat gray;
+        cv::cvtColor(img_correct, gray, cv::COLOR_BGR2GRAY);
+
+        // Noise Reduction
+        cv::medianBlur(gray, gray, 5);
+
+        std::vector<cv::Vec3f> circles;
+        cv::HoughCircles(gray, circles, cv::HOUGH_GRADIENT, 1, gray.rows/16, 100, 30, 1, 30);
+
+        if (circles.empty()) return cv::Point(-1,-1);
+         
+        // cogemos el primero
+        cv::Vec3i c = circles[0];
+
+        // Draw on Warped Image
+        cv::Point center = cv::Point(c[0], c[1]);
+        // draw marker on center
+        cv::drawMarker(img_correct, center, cv::Scalar(0,0,255), cv::MARKER_CROSS);
+        // draw circle
+        int radius = c[2];
+        cv::circle( img_correct, center, radius, cv::Scalar(0,0,255), 3, cv::LINE_AA);
+
+        // Transform to original image
+        cv::Point pt_r = ptTfPerspective(cv::Point(center.x + radius, center.y)); // punto en la circunferencia del circulo
+        cv::Point center_o = ptTfPerspective(center);
+        float r = cv::norm(pt_r - center_o);
+        
+        // Draw on Original Image
+        cv::drawMarker(img, center_o, cv::Scalar(0,0,255), cv::MARKER_CROSS);
+        cv::circle( img, center_o, r, cv::Scalar(0,0,255), 3, cv::LINE_AA);
+
+        return center_o;
+    }
+
+    void markers_img_callback(const campero_ur10_msgs::ArucoMarkersImg& msg) {
+        cv_bridge::CvImagePtr cv_ptr;
+        try {
+            cv_ptr = cv_bridge::toCvCopy(msg.img, sensor_msgs::image_encodings::BGR8);
+            cv::Mat img = cv_ptr->image;
+
+            //campero_ur10_msgs::ArucoMarkerArray markers = msg.markers;
+            std::vector<campero_ur10_msgs::ArucoMarker> markers = msg.markers.markers;
+            
+            cv::Mat img_correct = img.clone();
+            correctImage(markers, img_correct);
+
+            cv::Point pt = test(img, img_correct);
+            if (pt.x == -1) {
+                ROS_WARN("Test failed");
+            } else {
+                // 1.Obtener 4 puntos esquinas imagen
+                // 2.Obtener 4 puntos esquinas imagen
+                // 3.obtener de la camara -> camaraMatrix y distMatrix
+                
+                /*geometry_msgs::Pose p = markers[0].pose;
+                tf::Transform world_ref;
+                tf::poseMsgToTF(p, world_ref);
+                
+                geometry_msgs::Vector3 v;
+                tf::vector3TFToMsg(world_ref.getOrigin(),v);
+                std::cout << v << std::endl;
+                std::cout << p << std::endl;
+
+                
+                tf::Vector3 t = world_ref * world_ref.getOrigin();
+                tf::vector3TFToMsg(t,v);
+                tf::Quaternion q = world_ref.getRotation();
+                tf::Matrix3x3 m(q);
+
+                double r, p, y;
+                m.getRPY(r,p,y);
+
+                cv::Mat rvec = (cv::Mat_<double>(1,3))
+                std::cout << v << std::endl;*/
+            }
+            
+
+            // Publish Result Image
+            if (image_res_pub.getNumSubscribers() > 0) {
+                cv_bridge::CvImage out_msg;
+                out_msg.encoding = sensor_msgs::image_encodings::BGR8;
+                out_msg.image = img;
+                image_res_pub.publish(out_msg.toImageMsg());
+            }
+
+            // Publish Debug Image
+            if (image_debug_pub.getNumSubscribers() > 0) {
+                cv_bridge::CvImage out_msg;
+                out_msg.encoding = sensor_msgs::image_encodings::BGR8;
+                out_msg.image = img_correct;
+                image_debug_pub.publish(out_msg.toImageMsg());
+            }
+        }
+        catch (cv_bridge::Exception& e)
+        {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
     }
 };
 
